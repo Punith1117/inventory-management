31f822e204635eb91656f0ad0087063f5cf78a7b
Punith1117 <punith11172006@gmail.com>
2025-05-03 20:52:04 +0530

    add relations table to relate expertise and players and default values

--- db/populate.js
+++ db/populate.js
@@ -41,6 +41,41 @@ INSERT INTO players (name) VALUES
     ('M S Dhoni')
 ;
 
+CREATE TABLE IF NOT EXISTS expertise_player (
+    expert_in_id INTEGER NOT NULL,
+    player_id INTEGER NOT NULL,
+    PRIMARY KEY (expert_in_id, player_id),
+    FOREIGN KEY (expert_in_id) REFERENCES expertise(expert_in_id) ON DELETE CASCADE,
+    FOREIGN KEY (player_id) REFERENCES players(player_id) ON DELETE CASCADE
+);
+
+INSERT INTO expertise_player (expert_in_id, player_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (1, 3),
+    (1, 4),
+    (1, 5),
+    (2, 6),
+    (2, 7),
+    (2, 8),
+    (2, 9),
+    (2, 10),
+    (3, 11),
+    (3, 12),
+    (3, 13),
+    (3, 14),
+    (3, 15),
+    (4, 16),
+    (4, 10),
+    (4, 3),
+    (4, 5),
+    (4, 17),
+    (5, 5),
+    (5, 17),
+    (5, 18),
+    (5, 16)
+;
 `;
 
 async function main() {
