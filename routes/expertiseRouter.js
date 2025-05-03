6e6ef0d243c26ad34e2393f17077537ebecc0190
Punith1117 <punith11172006@gmail.com>
2025-05-03 21:37:10 +0530

    add code to view players expert in a particular field

--- routes/expertiseRouter.js
+++ routes/expertiseRouter.js
@@ -3,9 +3,15 @@ const db = require('../db/queries')
 const expertiseRouter = Router()
 
 expertiseRouter.get('/', async (req, res) => {
-    const expertise = await db.getAllExpertise()
-    const players = await db.getAllPlayers()
-    res.render('allPlayers', {expertise: expertise, players: players})
+    if ((req.query.expert_in == 'All') || (req.query.expert_in == undefined)) {        
+        const expertise = await db.getAllExpertise()
+        const players = await db.getAllPlayers()
+        res.render('allPlayers', {expertise: expertise, players: players})
+    } else {
+        const allExpertise = await db.getAllExpertise()
+        const players = await db.getPlayersExpertIn(req.query.expert_in)
+        res.render('playersExpert_in', {expertise: req.query.expert_in, players: players, allExpertise: allExpertise})
+    }
 })
 
 module.exports = expertiseRouter
