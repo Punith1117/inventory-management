b55acfa6179152273034808d32e541efaea650f6
Punith1117 <punith11172006@gmail.com>
2025-05-03 21:34:11 +0530

    add query to get players expert in a particular field

--- db/queries.js
+++ db/queries.js
@@ -26,11 +26,25 @@ async function destroyExpertise(expert_in) {
     await pool.query('DELETE FROM expertise WHERE expert_in = ($1)', [expert_in])
 }
 
+async function getPlayersExpertIn(expert_in) {
+    const queryArray = [
+        'SELECT expert_in, name FROM expertise_player AS ep',
+        'JOIN expertise AS e ON e.expert_in_id = ep.expert_in_id',
+        'JOIN players AS p ON p.player_id = ep.player_id',
+        'WHERE e.expert_in = ($1)',
+        ';'
+    ]
+    const query = queryArray.join(' ')
+    const { rows } = await pool.query(query, [expert_in])
+    return rows
+}
+
 module.exports = {
     getAllExpertise,
     getAllPlayers,
     addNewPlayer, 
     addNewExpertise,
     destroyPlayer,
-    destroyExpertise
+    destroyExpertise,
+    getPlayersExpertIn
 }
