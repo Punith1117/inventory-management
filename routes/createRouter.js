14125c41db5dfad1788f2fb3d85640a02bffb47b
Punith1117 <punith11172006@gmail.com>
2025-05-04 16:56:23 +0530

    add validation to input value of new player/expertise by using express validator

--- routes/createRouter.js
+++ routes/createRouter.js
@@ -1,18 +1,24 @@
 const Router = require('express')
 const db = require('../db/queries')
 const createRouter = Router()
+const { body, validationResult } = require('express-validator');
 
 createRouter.get('/', (req, res) => {
     res.render('create')
 })
 
-createRouter.post('/', async (req, res) => {
-    if (req.body.type == 'player') {
-        await db.addNewPlayer(req.body.createValue)
+createRouter.post('/', body('createValue').trim().notEmpty().withMessage('Expertise/Player cannot be empty'), async (req, res) => {
+    let result = validationResult(req)
+    if (result.isEmpty()) {
+        if (req.body.type == 'player') {
+            await db.addNewPlayer(req.body.createValue)
+        } else {
+            await db.addNewExpertise(req.body.createValue)
+        }
+        res.redirect('/expertise')
     } else {
-        await db.addNewExpertise(req.body.createValue)
+        res.render('error', {errors: result.array()})
     }
-    res.redirect('/expertise')
 })
 
 module.exports = createRouter
