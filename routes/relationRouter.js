29a21b6667184d4394cc3d3d18afc8f90df3fe81
Punith1117 <punith11172006@gmail.com>
2025-05-04 14:14:45 +0530

    add code to handle removal of relation between player and expertise

--- routes/relationRouter.js
+++ routes/relationRouter.js
@@ -7,4 +7,8 @@ relationRouter.post('/:expert_in_id/add/', async (req, res) => {
     res.redirect(`/expertise/${req.params.expert_in_id}`)
 })
 
+relationRouter.post('/:expert_in_id/remove/:player_id', async (req, res) => {
+    await db.removePlayerFromExpertise(req.params.expert_in_id, req.params.player_id)
+    res.redirect(`/expertise/${req.params.expert_in_id}`)
+})
 module.exports = relationRouter
